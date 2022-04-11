a1f684b1cfc855e98acbfe967cc4501d60a4c0a9
jan <131100fp@gmial.com>
2022-04-11 22:20:22 +0800

    first

--- src/com/smartcampus/dao/GradeDao.java
+++ src/com/smartcampus/dao/GradeDao.java
@@ -30,15 +30,15 @@ public class GradeDao {
     public GradeDao() {
     }
 
-    public List<Grade> getGrade(String Sid, int year, int semester) {
+    public List<Grade> getGrade(String sid, int year, int semester) {
         //如果semester为 0 需要返回一整个学期的成绩，也即查询年份为 year 学期为2 年份为 year + 1 学期为1
         //如果semester为 1 需要返回整个学年的第一个学期成绩，也即年份为 year 学期为2的结果
         //如果semester为 2 需要返回整个学年的第二个学期成绩，也即年份为year + 1 学期为1的结果
         String sql = null;
-        if (semester == 0)
-            sql = "SELECT Cname, Type,Credit,Grade FROM s_c,course\n" +
-                    "WHERE s_c.Cid = course.Cid AND s_c.Sid = ? AND s_c.Year = ? AND s_c.Semester=?;";
-        List<Grade> list = jdbcTemplate.query(sql, new GradeMAapper(), Sid, year, semester);
+//        if (semester == 0)
+        sql = "SELECT Cname, Type,Credit,Grade FROM s_c,course\n" +
+                "WHERE s_c.Cid = course.Cid AND s_c.Sid = " + " AND s_c.Year = ? AND s_c.Semester=?;";
+        List<Grade> list = jdbcTemplate.query(sql, new GradeMAapper());
         System.out.println(list);
         return list;
     }
